b81037575ec760546aacc662d158885ddfd28ab6
Rawknee.69 <65098395+Rawknee-69@users.noreply.github.com>
2024-04-13 12:12:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ def get_version(*file_paths):
 
 
 setup(
-    name='netlify_py',
-    version=get_version("netlify_py", "__init__.py"),
+    name='netlifyup_py',
+    version=get_version("netlifyup_py", "__init__.py"),
     packages=find_packages(),
     include_package_data=True,
     install_requires=["requests"],
@@ -31,9 +31,9 @@ setup(
     description='A python wrapper for creating, managing and deploying sites to Netlify using the Netlify APIs.',
     long_description=README,
     long_description_content_type="text/markdown",
-    url='https://github.com/akshay-realiq/netlify_py',
-    author='akshay kumar c',
-    author_email='akshay@realiq.io',
+    url='https://github.com/Rawknee-69/mmap_update.git',
+    author='Rawknee.69',
+    author_email='rawknee.6069@gmail.com',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
