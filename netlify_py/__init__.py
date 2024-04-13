a4d6fe3579a569269726ba06db6991a292b7cc7c
Rawknee-69 <darchana827@gmail.com>
2024-04-13 12:40:29 +0530

    first commit

--- netlify_py/__init__.py
+++ netlify_py/__init__.py
@@ -2,4 +2,4 @@
 __version__ = '0.1.0'
 
 from .apis.exceptions import NetlifyPyError
-from .netlifyup_py import NetlifyPy
+from .netlify_py import NetlifyPy
--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ import re
 
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -22,14 +19,13 @@ def get_version(*file_paths):
 
 
 setup(
-    name='netlifyup_py',
-    version=get_version("netlifyup_py", "__init__.py"),
+    name='netlify_up',
+    version=get_version("netlify_py", "__init__.py"),
     packages=find_packages(),
     include_package_data=True,
     install_requires=["requests"],
     license='MIT License',
     description='A python wrapper for creating, managing and deploying sites to Netlify using the Netlify APIs.',
-    long_description=README,
     long_description_content_type="text/markdown",
     url='https://github.com/Rawknee-69/mmap_update.git',
     author='Rawknee.69',
